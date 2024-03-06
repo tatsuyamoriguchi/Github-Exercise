93f713fd5b16e093a74fcf8187f4d73a3399e8d4
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-03-06 09:47:13 -0800

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Tatsuya Moriguchi on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
